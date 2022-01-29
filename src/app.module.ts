04526ead5661ef9dfbfc3268ac1502f1f50788e3
VIREAK KEOSOKVIBOL <vireakvibol@gmail.com>
2022-01-29 15:34:56 +0700

    Add installer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InstallerModule } from './installer/installer.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'billbox_authentication',
+      synchronize: false,
+      autoLoadModels: true,
+      logging: true
+    }),
+    InstallerModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/options/options.model.ts
@@ -0,0 +1,25 @@
+import { Column, Model, Table } from 'sequelize-typescript';
+import { Optional } from 'sequelize';
+import { UUIDV4 } from 'sequelize';
+
+interface Option {
+  readonly id: string,
+  readonly name: string,
+  readonly value: string,
+}
+
+interface OptionOptional extends Optional<Option, 'id'> { }
+
+@Table({ tableName: 'options', createdAt: 'created_at', updatedAt: 'updated_at' })
+export class OptionsModel extends Model<Option, OptionOptional> {
+
+  @Column({ primaryKey: true, defaultValue: UUIDV4 })
+  readonly id: string;
+
+  @Column({ allowNull: false, unique: true })
+  readonly name: string;
+
+  @Column({ allowNull: false })
+  readonly value: string;
+
+}
