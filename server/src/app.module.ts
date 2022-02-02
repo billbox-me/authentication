40550721f838f1ded50be7264ffa067e1f4b24c2
VIREAK KEOSOKVIBOL <vireakvibol@gmail.com>
2022-02-02 16:42:56 +0700

    reonfig build and file structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize/types';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InstallerModule } from './installer/installer.module';
+import { production, development} from 'database/config/database.json';
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'billbox_authentication',
-      synchronize: false,
-      autoLoadModels: true,
-      logging: true
+      dialect: <Dialect>'mysql'
     }),
     InstallerModule
   ],
